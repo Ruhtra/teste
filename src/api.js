97b691f1fb14faf7e3772af859094ab15c780408
Ruhtra <kawanarthurskate@gmail.com>
2023-06-30 10:30:31 -0300

    first

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const serverless = require('serverless-http')
+
+const app = express()
+
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+
+	res.json({
+		"msg": "hello"
+	})
+})
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app)
